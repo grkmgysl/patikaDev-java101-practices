89dbf948a028d05fbfa08f4a4d77b7de1f901558
grkmgysl <ggoysal@hotmail.com>
2022-08-15 16:11:40 +0300

    averageGrade added

--- averageGrade/src/averageGrade.java
+++ averageGrade/src/averageGrade.java
@@ -1,5 +1,41 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class averageGrade {
     public static void main(String[] args) {
+        //variables
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        //scanner
+        Scanner scan = new Scanner(System.in);
+
+        //get inputs
+        System.out.println("Matematik notu: ");
+        mat = scan.nextInt();
+
+        System.out.println("Fizik notu: ");
+        fizik = scan.nextInt();
+
+        System.out.println("Kimya notu: ");
+        kimya = scan.nextInt();
+
+        System.out.println("Turkce notu: ");
+        turkce = scan.nextInt();
+
+        System.out.println("Tarih notu: ");
+        tarih = scan.nextInt();
+
+        System.out.println("Muzik notu: ");
+        muzik = scan.nextInt();
+
+        //average calculation
+        int sum = mat + fizik + kimya + turkce + tarih + muzik;
+        double average = sum / 6.0;
+
+        System.out.println("Ortalamaniz" + average);
+
+        String isPassed = average > 60 ? "geçti" : "kaldi";
+        System.out.println(isPassed);
 
     }
 }
