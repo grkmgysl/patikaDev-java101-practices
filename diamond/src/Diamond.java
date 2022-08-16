b78e626421347d54096a4694f82ef3bd37621bcb
grkmgysl <ggoysal@hotmail.com>
2022-08-16 13:16:23 +0300

    added diamond

---
+++ diamond/src/Diamond.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String[] args) {
+
+        int n;
+
+        //scanner
+        Scanner scan = new Scanner(System.in);
+
+        //get inputs
+        System.out.println("Sayi girin : ");
+        n = scan.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n-1; i >= 0 ; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
